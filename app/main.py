23ce4839903d74832694af28ae4ce00bbd966330
Jordane GENGO <jgengo@student.42.fr>
2018-12-14 15:59:30 +0100

    update method

--- app/main.py
+++ app/main.py
@@ -3,23 +3,22 @@ import falcon
 import qrcode
 from io import BytesIO
 
-def create_qr():
+def create_qr(data=""):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_M,
         box_size=7,
         border=1,
     )
-    return qr
+
+    qr.add_data(data)
+    qr.make(fit=True)
+    return qr.make_image(fill_color="black", back_color="white")
 
 class QRCode:
     def on_get(self, req, res):
         if req.get_param('data'):
-            qr = create_qr()
-            qr.add_data(req.get_param('data'))
-            qr.make(fit=True)
-
-            img = qr.make_image(fill_color="black", back_color="white")
+            img = create_qr(req.get_param('data'))
 
             io = BytesIO()
             img.save(io, "PNG")
